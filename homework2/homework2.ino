353943a5b56b6cf3d3a26bafabe9bdd93a129cc9
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-10-29 17:57:34 +0200

    add second version homework2

--- homework2/homework2.ino
+++ homework2/homework2.ino
@@ -18,10 +18,17 @@ const int operationalElevatorLedPin = 8;
 byte operationalElevatorLedState = LOW;
 unsigned long previousTimeForElevatorLed = 0;
 
+unsigned long timeElevatorButtonPressed = 0;
 unsigned long timeElevatorStartedMoving = 0;
 byte isElevatorMoving = 0;
 int currentFloor = 0;
 int floorsTraveled = 0;
+int destination = 0;
+
+const int movingBetweenTwoFloorsDuration = 3000;
+const int durationPerFloor = 1000;
+const int operationalLedBlinkDuration = 500;
+const int closingElevatorDoorsDuration = 1500;
 
 const int floor0LedPin = 10;
 const int floor1LedPin = 11;
@@ -30,6 +37,12 @@ const int floor2LedPin = 12;
 byte floor0LedState = LOW;
 byte floor1LedState = LOW;
 byte floor2LedState = LOW;
+
+unsigned long previousTimeLed0 = 0;
+unsigned long previousTimeLed1 = 0;
+unsigned long previousTimeLed2 = 0;
+
+unsigned long currentMillis = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -43,19 +56,17 @@ void setup() {
 }
 
 void loop() {
+  currentMillis = millis();
+
   buttonStateFloor0 = getButtonState(buttonPinFloor0, buttonStateFloor0, lastButtonStateFloor0, lastDebounceTimeFloor0);
   buttonStateFloor1 = getButtonState(buttonPinFloor1, buttonStateFloor1, lastButtonStateFloor1, lastDebounceTimeFloor1);
   buttonStateFloor2 = getButtonState(buttonPinFloor2, buttonStateFloor2, lastButtonStateFloor2, lastDebounceTimeFloor2);
 
+  elevator();
+
   if (isElevatorMoving) {
-    blinkLed(operationalElevatorLedPin, operationalElevatorLedState, previousTimeForElevatorLed, 500);
-    if (timeElevatorStartedMoving != 0 && millis() > timeElevatorStartedMoving + 3000 * floorsTraveled) {
-      isElevatorMoving = 0;
-    }
     return;
   }
-
-  digitalWrite(operationalElevatorLedPin, LOW);
 
   if (currentFloor == 0) {
     digitalWrite(floor0LedPin, HIGH);
@@ -66,34 +77,69 @@ void loop() {
   }
 
   if (buttonStateFloor0 == 1 && currentFloor != 0) {
-    moveElevator(currentFloor, 0);
+    moveElevatorToFloor(0);
   } 
   if (buttonStateFloor1 == 1 && currentFloor != 1) {
-    moveElevator(currentFloor, 1);
+    moveElevatorToFloor(1);
   }
   if (buttonStateFloor2 == 1 && currentFloor != 2) {
-    moveElevator(currentFloor, 2);
+    moveElevatorToFloor(2);
   }  
 }
 
-void moveElevator(int source, int dest) {
+void moveElevatorToFloor(int destinationFloor) {
   if (isElevatorMoving) {
     return;
   }
 
-  floorsTraveled = abs(dest - source);
+  timeElevatorButtonPressed = currentMillis;
 
-  if (source == 0) {
-    digitalWrite(floor0LedPin, LOW);
-  } else if (source == 1) {
-    digitalWrite(floor1LedPin, LOW);
-  } else {
-    digitalWrite(floor2LedPin, LOW);
-  }
-
-  timeElevatorStartedMoving = millis();
+  destination = destinationFloor;
   isElevatorMoving = 1;
-  currentFloor = dest;
+}
+
+void elevator() {
+  if (isElevatorMoving) {
+    if (currentMillis - timeElevatorButtonPressed < closingElevatorDoorsDuration) {
+      updateLedAtFloor(currentFloor, HIGH);
+      return;
+    } else {
+      updateLedAtFloor(currentFloor, LOW);
+      timeElevatorStartedMoving = timeElevatorButtonPressed + closingElevatorDoorsDuration;
+    }
+
+    blinkLed(operationalElevatorLedPin, operationalElevatorLedState, previousTimeForElevatorLed, operationalLedBlinkDuration);
+
+    floorsTraveled = abs(destination - currentFloor);
+
+    if (floorsTraveled == 2) {
+      if (timeElevatorStartedMoving != 0 && 
+          currentMillis > timeElevatorStartedMoving + movingBetweenTwoFloorsDuration &&
+          currentMillis < timeElevatorStartedMoving + movingBetweenTwoFloorsDuration + durationPerFloor
+      )
+        blinkLed(floor1LedPin, floor1LedState, previousTimeLed1, durationPerFloor);
+      else
+        digitalWrite(floor1LedPin, 0);
+    }
+
+    if (currentMillis > timeElevatorStartedMoving + movingBetweenTwoFloorsDuration * floorsTraveled) {
+      isElevatorMoving = 0;
+      currentFloor = destination;
+    }
+    return;
+  }
+  
+  digitalWrite(operationalElevatorLedPin, LOW);
+}
+
+void updateLedAtFloor(int floor, byte newState) {
+  if (floor == 0) {
+    digitalWrite(floor0LedPin, newState);
+  } else if (floor == 1) {
+    digitalWrite(floor1LedPin, newState);
+  } else {
+    digitalWrite(floor2LedPin, newState);
+  }
 }
 
 int getButtonState(int buttonPin, byte oldButtonState, byte& lastButtonState, unsigned long& lastDebounceTime) {
@@ -101,10 +147,10 @@ int getButtonState(int buttonPin, byte oldButtonState, byte& lastButtonState, un
   int reading = !digitalRead(buttonPin);
 
   if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+    lastDebounceTime = currentMillis;
   }
 
-  if (millis() - lastDebounceTime > buttonDebounceDelay) {
+  if (currentMillis - lastDebounceTime > buttonDebounceDelay) {
     if (reading != oldButtonState) {
       newButtonState = reading;
     }
@@ -114,21 +160,16 @@ int getButtonState(int buttonPin, byte oldButtonState, byte& lastButtonState, un
   return newButtonState;
 }
 
-void blinkLed(int ledPin, byte& ledState, unsigned long& previousTime , int blinkDuration) {
-  unsigned long currentMillis = millis();
+void blinkLed(int ledPin, byte& ledState, unsigned long& previousTime, int blinkDuration) {
   if (currentMillis - previousTime >= blinkDuration) {
-    Serial.println(currentMillis);
-    // save the last time you blinked the LED
     previousTime = currentMillis;
 
-    // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
 
-    // set the LED with the ledState of the variable:
     digitalWrite(ledPin, ledState);
   }
 }
